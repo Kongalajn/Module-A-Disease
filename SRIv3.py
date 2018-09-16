ad22d14853da7901d4c34e72868f453b9ae11d17
Kongalajn <42980699+Kongalajn@users.noreply.github.com>
2018-09-16 20:50:53 +0200

    Update SRIv3.py

--- SRIv3.py
+++ SRIv3.py
@@ -8,9 +8,13 @@ import RungeKutta4
 import numpy as np
 import matplotlib.pyplot as plt
 
+T_M=1/(S_M+I_M+R_M)
+
 def f(u,t,beta=0.000013,alpha=0.1,gamma=0.3):
     S, I, R = u
-    return [-beta*S*I - gamma*S, beta*S*I- alpha*I, gamma*S + alpha*I]
+    return [[S_M*(-beta*I_M - gamma + LM*T_M - MK*T_M), I_M*(beta*S_M- alpha + LM*T_M - MK*T_M), R_M(gamma*S_M + alpha*I_M + LM*T_M - MK*T_M)], #Malmö
+            [-beta*S*I - gamma*S - LM*T, beta*S*I- alpha*I - LM*T, gamma*S + alpha*I - LM*T], #Lund
+            [-beta*S*I - gamma*S + MK*T, beta*S*I- alpha*I + MK*T, gamma*S + alpha*I +MK*T]] #Köpenhamn
 
 n=60
 
